a27946cf820da373d573c6f7200f2c7933997058
Ashwina Sanil <ashwina13821@gmail.com>
2026-09-02 22:29:36 +0530

    Create __init__.py

---
+++ vehicle_simulation/control/__init__.py
@@ -0,0 +1,3 @@
+"""Control package: decision-making logic that sits between driver-state
+input (currently keyboard, later AI drowsiness detection) and the vehicle
+simulation (or, eventually, real hardware)."""
